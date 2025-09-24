8092e9c017e3a36b0f11989daa5ff3c7115f0273
Ingrid Mossige <143181732+imossige@users.noreply.github.com>
2025-09-24 18:38:00 +0200

    return tuple

--- possible_solutions.py
+++ possible_solutions.py
@@ -1,24 +1,39 @@
-# square a number
-def squared_number(number):
-    # return the squared number
-    return number**2 
 
 def solutions_diophantine_equation(upper_limit):
+    x = 0
+    y = 0
+
+    results = []
+
     for x in range(1,upper_limit+1):
         for y in range(1,upper_limit+1):
             for z in range(1,upper_limit+1):
                 # calculate the x^2 + y^2 and check if equal to z^2
                 if (x**2 + y**2 == z**2):
-                    # print (x,y,z) if equal
-                    print(x,y,z)
-    
+                    results.append([x,y,z])
+
+    return results
+"""
+
+1. Tuples vs list
+
+1. initialise 
+2. Loops
+3. Loops
+4. Loops
+5. Checks
+
+6. Prints
+"""   
 
 def main():
     # upper limit
     limit = 20
 
     # find solutions
-    solutions_diophantine_equation(limit)
+    triplets = solutions_diophantine_equation(limit)
+
+    print(triplets)
 
 if __name__ == "__main__":
     main()
