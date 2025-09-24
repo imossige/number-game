8b6b407a40f285726b371ee4814289165aba54d1
Ingrid Mossige <143181732+imossige@users.noreply.github.com>
2025-09-24 18:18:09 +0200

    add +1 to limit in range

--- possible_solutions.py
+++ possible_solutions.py
@@ -1,10 +1,12 @@
-# upper limit
-limit = 100
+# square a number
+def squared_number(number):
+    # return the squared number
+    return number**2 
 
 def solutions_diophantine_equation(upper_limit):
-    for x in range(1,upper_limit):
-        for y in range(1,upper_limit):
-            for z in range(1,upper_limit):
+    for x in range(1,upper_limit+1):
+        for y in range(1,upper_limit+1):
+            for z in range(1,upper_limit+1):
                 # calculate the x^2 + y^2 and check if equal to z^2
                 if (x**2 + y**2 == z**2):
                     # print (x,y,z) if equal
@@ -12,6 +14,11 @@ def solutions_diophantine_equation(upper_limit):
     
 
 def main():
+    # upper limit
+    limit = 20
+
+    # find solutions
     solutions_diophantine_equation(limit)
 
-main()
+if __name__ == "__main__":
+    main()
