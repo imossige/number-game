63a44de130dfa06dbac8339f14e0d9df795c8c6f
Ingrid Mossige <143181732+imossige@users.noreply.github.com>
2025-09-25 14:48:58 +0200

    final

--- test_pytagoras.py
+++ test_pytagoras.py
@@ -1,6 +1,12 @@
+import pytest
+
 from possible_solutions import solutions_diophantine_equation
 
 def test_pytagoras():
     
     assert solutions_diophantine_equation(0) == []
+    
+    with pytest.raises{TypeError}:
+        solutions_diophantine_equation(1)
+    
  
